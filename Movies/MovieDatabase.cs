70adff44fea8c0b9e1519d18f1205debd5ad21d6
hossamhafez12 <hossamhafez12@F0064-07-2329>
2019-11-18 16:59:32 -0600

    h

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -27,5 +27,39 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+        public List<Movie> SearchAndFilter(string searchString, List<string> rating)
+        {
+            if (searchString == null && rating.Count == 0) return null;
+            List<Movie> res = new List<Movie>();
+            foreach (Movie movie in movies)
+            {
+                if (searchString != null && rating.Count > 0)
+                {
+                    if (movie.Title != null
+                       && movie.Title != null && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase)
+                        && rating.Contains(movie.MPAA_Rating))
+                    {
+                        res.Add(movie);
+                    }
+                }
+                else if (searchString != null)
+                {
+                    if (movie.Title != null && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        res.Add(movie);
+                    }
+                }
+                else if (rating.Count > 0)
+                {
+                    if (rating.Contains(movie.MPAA_Rating))
+                    {
+                        res.Add(movie);
+                    }
+                }
+
+
+            }
+            return res;
+        }
     }
 }
